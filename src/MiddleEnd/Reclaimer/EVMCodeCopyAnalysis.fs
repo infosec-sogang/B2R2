1c0a6febb95fc34fb2ce66552b0ba9b995d1c964
Jaeseung Choi <jschoi.2022@gmail.com>
2021-08-02 17:04:02 +0900

    [MiddleEnd, EVM] Add temporary code copy analysis

--- src/MiddleEnd/Reclaimer/EVMCodeCopyAnalysis.fs
+++ src/MiddleEnd/Reclaimer/EVMCodeCopyAnalysis.fs
@@ -25,47 +25,54 @@
 namespace B2R2.MiddleEnd.Reclaimer
 
 open B2R2
-open B2R2.BinIR
-open B2R2.BinIR.LowUIR
-open B2R2.FrontEnd.BinLifter
+open B2R2.FrontEnd.BinFile
+open B2R2.FrontEnd.BinInterface
+open B2R2.FrontEnd.BinLifter.EVM
 open B2R2.MiddleEnd.BinEssence
 
 type EVMCodeCopyAnalysis () =
-  let findCodeCopy (ess: BinEssence) stmts =
-    stmts
-    |> Array.tryPick (fun stmt ->
-      match stmt.S with
-      | Store (_, { E = Num dst },
-                  { E = BinOp (BinOpType.APP, _len,
-                               { E = FuncName "code" },
-                               { E = BinOp (BinOpType.CONS, _,
-                                            { E = Num src }, _, _) }, _) }) ->
-        let dstAddr = BitVector.toUInt64 dst
-        let srcAddr = BitVector.toUInt64 src
-        let offset = srcAddr - dstAddr
-        (ess.BinHandle.Parser :?> EVM.EVMParser).CodeOffset <- offset
-        Some srcAddr
-      | _ -> None)
+
+  let updatePushList (evmIns: EVMInstruction) pushList =
+    match evmIns.Info.Opcode with
+    | Opcode.PUSH1 bv
+    | Opcode.PUSH2 bv
+    | Opcode.PUSH3 bv
+    | Opcode.PUSH4 bv -> BitVector.toUInt64 bv :: pushList
+    | _ -> []
+
+  let tryResolveOffset pushList =
+    match pushList with
+    | dstOffset :: srcOffset :: _ when dstOffset = 0UL -> Some (int srcOffset)
+    | _ -> None
+
+  // XXX. TODO: Use IR CFG to find and analyze codecopy instruction. Currently
+  // the basic block is not reachable in our CFG.
+  let rec findCodeCopyOffset hdl bp pushList =
+    if not (BinaryPointer.IsValid bp) then failwith "Failed to find codecopy"
+    match BinHandle.TryParseInstr (hdl, bp = bp) with
+    | Error _ -> failwith "Failed to find codecopy"
+    | Ok ins ->
+      let nextBP = BinaryPointer.Advance bp (int ins.Length)
+      let evmIns = ins :?> EVMInstruction
+      if evmIns.Info.Opcode = Opcode.CODECOPY then
+        match tryResolveOffset pushList with
+        | Some offset -> offset
+        | None -> findCodeCopyOffset hdl nextBP pushList
+      else findCodeCopyOffset hdl nextBP (updatePushList evmIns pushList)
 
   let recoverCopiedCode (ess: BinEssence) =
-    ess.InstrMap
-    |> Seq.fold (fun ess (KeyValue (_, ins)) ->
-      match ins.Stmts |> findCodeCopy ess with
-      | None -> ess
-      | Some addr ->
-        match ess.CalleeMap.Find addr with
-        | None ->
-          match BinEssence.addEntry ess (addr, ArchOperationMode.NoMode) with
-          | Ok ess -> ess
-          | Error _ -> Utils.impossible ()
-        | Some _ -> ess) ess
+    let hdl = ess.BinHandle
+    let initBp = hdl.FileInfo.ToBinaryPointer hdl.FileInfo.BaseAddress
+    let codeCopyOffset = findCodeCopyOffset hdl initBp []
+    let bytes = hdl.FileInfo.BinReader.Bytes
+    let len = bytes.Length
+    if len <= codeCopyOffset then failwith "Invalid codecopy offset"
+    let copiedBytes = bytes.[codeCopyOffset .. (len - 1)]
+    let newHdl = BinHandle.Init (hdl.FileInfo.ISA, copiedBytes)
+    BinEssence.init(newHdl)
 
   interface IAnalysis with
     member __.Name = "EVM Code Copy Analysis"
 
     member __.Run ess hint =
-      let ess =
-        match ess.BinHandle.FileInfo.ISA.Arch with
-        | Architecture.EVM -> recoverCopiedCode ess
-        | _ -> ess
-      ess, hint
+      recoverCopiedCode ess, hint
